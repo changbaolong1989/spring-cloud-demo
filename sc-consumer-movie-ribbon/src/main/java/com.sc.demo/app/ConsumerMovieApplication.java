1a7116acf8b4aab40f362b348f1563384bf80823
changbaolong <13940572922@163.com>
2019-11-21 10:26:57 +0800

    微服务集成Ribbon负载均衡

---
+++ sc-consumer-movie-ribbon/src/main/java/com.sc.demo/app/ConsumerMovieApplication.java
@@ -0,0 +1,15 @@
+package com.sc.demo.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com")
+public class ConsumerMovieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerMovieApplication.class, args);
+    }
+
+}
